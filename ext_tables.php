4817183d43a7e1435c8ec09dc0e13b78175e973a
Soren Malling <soren.malling@gmail.com>
2013-05-13 11:19:18 +0200

    [FEATURE] Use "Page Configuration" concept instead of static includes

--- ext_tables.php
+++ ext_tables.php
@@ -2,7 +2,7 @@
 if (!defined ('TYPO3_MODE')) 	die ('Access denied.');
 
 	# Add typoscript templates
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Page/', 'Leder.FDF.dk: Page setup');
+#t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/Page/', 'Leder.FDF.dk: Page setup');
 
 
 ?>
